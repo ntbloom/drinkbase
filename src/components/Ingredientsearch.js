a652d871f5a8e67258029b771c312c485d1d558f
Noah Bloom <ntbloom@gmail.com>
2018-11-06 21:35:45 -0500

    have rough idea of rendering entire arrays

--- src/components/Ingredientsearch.js
+++ src/components/Ingredientsearch.js
@@ -1,5 +1,35 @@
 import React, { Component } from "react";
 
+let drinksObj = {
+  'Bitter French': [
+    '1 oz gin',
+    '0.25 oz Campari',
+    '0.5 oz lemon juice',
+    '0.5 oz simple syrup',
+    '2.5 oz champagne'],
+  'Blackthorn': [
+    '1.5 oz gin',
+    '0.75 oz sweet vermouth',
+    '0.75 oz sloe gin',
+    '2 dashes orange bitters',
+    '1 garnish orange twist'],
+  'Dark & Stormy': [
+    '2 oz dark rum',
+    '0.75 oz lime juice',
+    '4 oz ginger beer',
+    '1 garnish lime wheel']
+}
+
+let drinksArr = [
+  'Bitter French',
+  'Blackthorn',
+  'Dark & Stormy']
+
+//logic for rendering html in loops
+const drinks = drinksArr.map((drinkName)  
+  <p>{drinkName}</p>
+);
+
 class Ingredientsearch extends Component {
   render() {
     return (
@@ -9,9 +39,10 @@ class Ingredientsearch extends Component {
         <ol>
           <li>form coming soon...</li>
         </ol>
-      </div> 
+      </div>, 
+      <p>{drinks}</p> //requires html tags
     );
   }
 }
 
-export default Ingredientsearch;
+export default Ingredientsearch
