fa1aabdfe0d2ddf74df6533c22b531db26e2ab6b
Noah Bloom <ntbloom@gmail.com>
2018-11-10 13:08:58 -0500

    finished prototype for DrinkBase class

--- backend/drinkStore.py
+++ backend/drinkStore.py
@@ -7,11 +7,22 @@ import sqlite3, json
 
 class DrinkBase:
     def __init__(self, database):
+        
+        # file name, needs to end in .db
         self.database = database
+        
         dbConnect = sqlite3.connect(
             self.database, check_same_thread=False)
         dbConnect.row_factory = lambda cursor, row: row[0]
+        
+        # self.cursor can be used to define any SQL query
+        # use self.cursor.fetchall() to actually run query
         self.cursor = dbConnect.cursor()
+        drinkNameQuery = self.cursor.execute(
+            'SELECT DISTINCT name FROM ingredients GROUP BY name')
+        
+        # set of every drink named in the database
+        self.allDrinks = set(sorted(self.cursor.fetchall()))
     
     def ingSearch(self, ingredient):
         '''populates set of drinks that contain 'ingredient'
@@ -31,11 +42,6 @@ class DrinkBase:
         drinks = self.cursor.fetchall()
         drinks = sorted(set(drinks))
         return drinks
-
-    def drinkSearch(self, included='', excluded='', drinkName=''):
-        '''returns set of drinks based on included ingredients, excluded
-        ingredients, or drink name'''
-        #TODO: define search algorithm
 
     def getRecipe(self, drink):
         '''returns full recipe for 'drink' variable in JSON format'''
@@ -57,12 +63,38 @@ class DrinkBase:
             unit = self.cursor.fetchall()
             unit = str(unit[0])
             recipe.append(amount + ' ' + unit + ' ' + i)
-        #TODO: jsonify drink recipes
-        return recipe
+        fullRecipe = json.dumps(dict(Name=drink, Ingredients=recipe))
+        return fullRecipe
+
+    def drinkSearch(self, included='', excluded='', drinkName=''):
+        '''returns set of drinks based on included ingredients, excluded
+        ingredients, or drink name'''
+        
+        # remember to send imcoming http request as Python list
+        drinks = self.allDrinks
+        if len(included) > 0:
+            for i in included:
+                drinks = drinks & set(self.ingSearch(i))
+        if len(excluded) > 0:
+            for i in excluded:
+                drinks = drinks - set(self.ingSearch(i))
+        if len(drinkName) > 0:
+            for i in drinkName:
+                drinks = drinks & set(self.nameSearch(i))
+        drinks = sorted(drinks) 
+        return drinks
+
 
 
 # for development/debugging
 db = DrinkBase('drinkBase.db')
+
+# drinkSearch
+#included = []
+#excluded = []
+#drinkName = ['old', 'fash']
+#ryeDrinks = db.drinkSearch(included, excluded, drinkName)
+#print(ryeDrinks)
 
 ## ingSearch
 #ryeDrinks = db.ingSearch('RYE')
@@ -72,8 +104,6 @@ db = DrinkBase('drinkBase.db')
 #frenchDrinks = db.nameSearch('french')
 #print(frenchDrinks)
 
-# getRecipe
-martinez = db.getRecipe('Manhattan')
-print(martinez)
-
-
+## getRecipe
+#martinez = db.getRecipe('Manhattan')
+#print(martinez)
