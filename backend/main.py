990b09367b2e30e813e3efe99eaffdf9936763f9
Noah Bloom <ntbloom@gmail.com>
2018-11-10 23:10:58 -0500

    have somewhat working api with url inputs

--- backend/main.py
+++ backend/main.py
@@ -12,19 +12,41 @@ ds = drinkStore.DrinkBase(database)
 
 app = Flask(__name__)
 
-@app.route('/')
-def render_api():
-    '''renders API in json format'''
-    allDrinks = sorted(ds.allDrinks)
+def cleanInput(string):
+    '''automatically puts included/excluded into list form for
+    rendering'''
     
-    #TODO: enter logic for included/excluded searches
+    #TODO: define this method. decide separator % or | or /
+    
+    return string
+
+
+@app.route(
+        '/api/v1.0/drinks/<included>/<excluded>/<name>', methods=['GET'])
+def render_api(included, excluded, name):
+    '''renders API in json format'''
+
+    drinkList = set(ds.allDrinks)
+    if included != '':   
+        incl = set(ds.ingSearch(included))
+        drinkList = drinkList & incl
+    if excluded != '':
+        excl = set(ds.ingSearch(excluded))
+        drinkList = drinkList - excl
+    #TODO: fix name logic
+    if name != '':
+        named = set(ds.nameSearch(name))
+        drinkList = drinkList & named
+    
+    #TODO: fix for multiple ingredients
+
     
     drinks = []
-    for drink in allDrinks: # change var name when algorithm introduced
+    for drink in drinkList: 
         recipe =  ds.getRecipe(drink)
         drinkDict = {'Name': drink, 'Recipe': recipe}
         drinks.append(drinkDict)
-    return jsonify(drinks)
+    return jsonify({'Drinks': drinks})
 
 if __name__ == '__main__':
     app.run(debug=True)
