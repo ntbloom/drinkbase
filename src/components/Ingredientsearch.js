e04e4d3bcef861371876f07d790f61337b0bf03c
Noah Bloom <ntbloom@gmail.com>
2018-11-13 22:13:49 -0500

    have basic form template for name search

--- src/components/Ingredientsearch.js
+++ src/components/Ingredientsearch.js
@@ -5,35 +5,6 @@
 
 import React, { Component } from "react";
 
-let drinksObj = {
-  'Bitter French': [
-    '1 oz gin',
-    '0.25 oz Campari',
-    '0.5 oz lemon juice',
-    '0.5 oz simple syrup',
-    '2.5 oz champagne'],
-  'Blackthorn': [
-    '1.5 oz gin',
-    '0.75 oz sweet vermouth',
-    '0.75 oz sloe gin',
-    '2 dashes orange bitters',
-    '1 garnish orange twist'],
-  'Dark & Stormy': [
-    '2 oz dark rum',
-    '0.75 oz lime juice',
-    '4 oz ginger beer',
-    '1 garnish lime wheel']
-}
-
-let drinksArr = [
-  'Bitter French',
-  'Blackthorn',
-  'Dark & Stormy']
-
-//logic for rendering html in loops
-const drinks = drinksArr.map((drinkName) =>
-  <p>{drinkName}</p>
-);
 
 class Ingredientsearch extends Component {
   render() {
@@ -41,11 +12,7 @@ class Ingredientsearch extends Component {
       <div>
         <h2>SEARCH BY INGREDIENT</h2>
         <p>:: enter ingredients you'd like to include in your drinks ::</p>
-        <ol>
-          <li>form coming soon...</li>
-        </ol>
-      </div>, 
-      <p>{drinks}</p> //requires html tags
+      </div>
     );
   }
 }
--- src/components/Namesearch.js
+++ src/components/Namesearch.js
@@ -14,9 +14,48 @@ class Namesearch extends Component {
         <ol>
           <li>form coming soon...</li>
         </ol>      
-      </div>
+      </div>,
+      <Nameform /> // how to refer to components inside others
     );
   }
 }
 
+class Nameform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this); 
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="searchForm">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            :: enter the name of a drink ::
+            <input 
+              type="text" 
+              onChange={this.handleChange}
+              value={this.state.value}
+              placeholder=" ex: martinez"/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      );
+    }
+  }
 export default Namesearch;
