33204a793009d5303626714dcf692ba00a08c6ba
Noah Bloom <ntbloom@gmail.com>
2018-11-15 11:08:43 -0500

    changed Results to props

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import Nameform from "./Nameform";
+
 
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drinks: {}
+    }
+
+
   render() {
     return (
       <div>
         <h2>TRY ONE OF THESE</h2>
       
-        //insert code for rendering drinks
-      
+        //insert code here
       
       </div>
     );
