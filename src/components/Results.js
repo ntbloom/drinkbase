12d39a406c35309a6e0eed99da79bf3685be0468
Noah Bloom <ntbloom@gmail.com>
2018-11-19 23:06:03 -0500

    reorganized api call. Still not working properly.

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,32 +1,45 @@
 import React, { Component } from "react";
-//import Nameform from "./Nameform";
-
+//import Namesearch from "./Namesearch";
 
 class Results extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drinks: [],
-      things: props.things
+      //query: this.props.drinks,
+      query: "martinez",
+      drinks: {}
+    };
+    console.log("constructor call : ", this.state.query);
+    this.apiCall = this.apiCall.bind(this);
+  }
+
+  
+
+  apiCall() {
+    var xhr = new XMLHttpRequest();
+    var apiResponse = {};
+    var url = "http://localhost:5000/api/v1.0/names/?name=";
+    xhr.open("GET", url.concat(this.state.query), true);
+    xhr.send();
+    xhr.onload = function(e) {
+      if (xhr.status === 200) {
+        apiResponse = JSON.parse(xhr.response)
+        console.log("xhr200 : ", xhr.status, apiResponse)
+      } else {
+        apiResponse = xhr.statusText
+        console.error("xhrerror : ", xhr.statusText)
+      }
     }
+    this.setState({drinks: apiResponse});
+    console.log("end of API call : ", this.state.drinks);
   }
-
-  createThing(item) {
-    console.log(item);
-    return <li key={item}>{item}</li>
-  }
-
+  
   render() {
-    console.log("state things", this.state.things);
-    var things = this.state.things.map(this.createThing);
-    console.log("thin", things);
+    console.log("render this.state.query : ", this.state.query);
   
     return (
       <div>
         <h2>TRY ONE OF THESE</h2>
-        <ul>
-          {things}
-        </ul>
       </div>
     );
   }
