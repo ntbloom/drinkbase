76c778c29e2b3c0644cbc8d119602e9d8564e583
Noah Bloom <ntbloom@gmail.com>
2018-12-10 22:35:21 -0500

    added numbers to tests for debugging

--- API/tests/api_test.py
+++ API/tests/api_test.py
@@ -17,68 +17,68 @@ def api(url):
 class TestNameSearch(unittest.TestCase):
     '''tests nameSearch function'''
     # name tests 
-    def test_single_name(self):
+    def test_single_name1(self):
         self.assertEqual(
             api("names/?name=bitter french"),
             ["Bitter French"]
             )
-    def test_multiple__responses(self):
+    def test_multiple__responses2(self):
         self.assertEqual(
             api("names/?name=negroni"), 
             ["Negroni", "Summer Negroni"]
             )
     
     # included tests
-    def test_single_incl_single(self):
+    def test_single_incl3(self):
         self.assertEqual(
             api("ingreds/?incl=heering"),
             ["Blood & Sand"]
             )
-    def test_more_single_incl(self):
+    def test_more_single_incl4(self):
         self.assertEqual(
             api("ingreds/?incl=cocchi"),
             ["20th Century", "Vesper"]
             )
-    def test_multiple_incl(self):
+    def test_multiple_incl5(self):
         self.assertEqual(
             api("ingreds/?incl=rye,grapefruit"),
             ["Brown Derby"]
             )
-    def test_more_multiple_incl(self):
+    def test_more_multiple_incl6(self):
         self.assertEqual(
-            api("ingreds/?incl=apple,cinnamon"),
-            ["Apple Toddy"]
+            api("ingreds/?incl=apple,mint"),
+            ["Jersey Julep"]
             )
     
     # excluded tests
-    def test_single_excl(self):
+    def test_single_excl7(self):
         self.assertEqual(
             api("ingreds/?excl=garnish"),
             noGarnish
             )
-    def test_multiple_excl(self):
+    def test_multiple_excl8(self):
         self.assertEqual(
             api("ingreds/?excl=juice,bitters"),
             noJuiceBitters
             )
     
     # excluded & included tests
-    def test_single_incl_and_excl(self):
+    def test_single_incl_and_excl9(self):
         self.assertEqual(
             api("ingreds/?incl=fernet&excl=whiskey"),
             ["Hanky Panky"]
             )
-    def test_multiple_incl_single_excl(self):
+    def test_multiple_incl_single_excl10(self):
         self.assertEqual(
             api("ingreds/?incl=rye,vermouth&excl=bitters"),
             ["Boulevardier", "Old Pal", "Scofflaw Cocktail"]
             )
-    def test_single_incl_multiple_excl(self):
+    def test_single_incl_multiple_excl11(self):
         self.assertEqual(
             api("ingreds/?incl=grapefruit&excl=tequila,rum"),
             ["Blonde Redhead", "Brown Derby"]
             )
-    def test_multiple_incl_multiple_excl(self):
+    def test_multiple_incl_multiple_excl12(self):
         self.assertEqual(
             api("ingreds/?incl=rye,vermouth&excl=bitters,juice"),
             ["Boulevardier", "Old Pal"]
