573a65e226e1a118d1ccce4808ab29be4cb95815
Noah Bloom <ntbloom@gmail.com>
2018-12-12 22:32:49 -0500

    added just included form. needs improvement

--- src/components/Ingredientsearch.js
+++ src/components/Ingredientsearch.js
@@ -6,21 +6,75 @@
 import React, { Component } from "react";
 import Results from "./Results";
 
-const url = "http://localhost:5000/api/v1.0/ingreds/";
+const url = "http://localhost:5000/api/v1.0/ingreds/?incl=";
+let query = ""
 
 class Ingredientsearch extends Component {
-  //TODO: call props and this.state
-  //TODO: create forms
-  //TODO: format api call
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      included: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
+  }
+  handleChange(event) {
+    this.setState({included: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({submitted: true})
+    query = this.state.included;
+    event.preventDefault(); 
+  }
   render() {
-    return (
-      <div>
-        <h2>SEARCH BY INGREDIENT</h2>
-        <p>:: enter ingredients you'd like to include in your drinks ::</p>
+    if (this.state.submitted === true) {
+      return (
+        <div className="namesearchMain">
+          <div>
+            <h2>SEARCH BY INGREDIENT</h2>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              :: enter an ingredient to include ::
+              <input
+                type="text"
+                value={this.state.included}
+                onChange={this.handleChange}
+                placeholder=" ex: rye" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <div>
+            <Results 
+              query={this.state.included} 
+              url={url}
+            />
+          </div>
+        </div>
       </div>
-    );
+      );
+    } else {
+      return (
+        <div className="namesearchMain">
+          <div>
+            <h2>SEARCH BY INGREDIENT</h2>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              :: enter an ingredient to include ::
+              <input
+                type="text"
+                value={this.state.included}
+                onChange={this.handleChange}
+                placeholder=" ex: vermouth" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+      );
+    }
   }
 }
+
 
 export default Ingredientsearch
