771f600836d938a4b8d62736da19b69067e76531
Noah Bloom <ntbloom@gmail.com>
2018-12-20 21:34:21 -0500

    have proper rendering of forms with plus and minus button

--- src/components/menubar/additions/Plusminus.js
+++ src/components/menubar/additions/Plusminus.js
@@ -1,7 +1,6 @@
 // Plusminus, adds or subtracts ingredientadd rows
 
 import React, { Component } from "react";
-import Ingredientadd from "./Ingredientadd";
 
 class Plusminus extends Component {
   constructor(props) {
@@ -12,16 +11,63 @@ class Plusminus extends Component {
     };
     this.addRow = this.addRow.bind(this);
     this.deleteRow = this.deleteRow.bind(this);
+    this.ingredRow = this.ingredRow.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   
   addRow() {
     const qty = this.state.rows;
     this.setState({rows: qty + 1})
+    
   }
 
   deleteRow() {
     const qty = this.state.rows;
-    this.setState({rows: qty - 1})
+    if (qty > 0) {
+      this.setState({rows: qty - 1})
+    }
+  }
+
+  ingredRow() {
+    let rows = []
+    for (let i = 0; i < this.state.rows; i++) {
+      rows.push(
+        <div>
+          <input
+            key={"ing" + i}
+            type="text"
+            placeholder=" ingredient name"
+            name="Ingredient"
+            onChange={this.handleChange}
+          >
+          </input>
+          <input
+            key={"amt" + i}
+            type="text"
+            placeholder=" amount (decimals)"
+            name="Amount"
+            onChange={this.handleChange}
+          >
+          </input>
+          <select
+            key={"unit" + i}
+            name="Unit"
+            onChange={this.handleChange}
+          >
+            <option value="oz">oz</option>
+            <option value="dashes">dashes</option>
+            <option value="each">each</option>
+            <option value="garnish">garnish</option>
+          </select>
+        </div>
+      )
+    }
+    return <div>{rows}</div>
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+    console.log("state: ", this.state);
   }
 
   render() {
@@ -32,17 +78,39 @@ class Plusminus extends Component {
           <button type="submit" onClick={this.addRow}>[+]</button>
           <button type="sumbit" onClick={this.deleteRow}>[-]</button>
         </form>
-        <span id="1"><Ingredientadd /></span>
-        <span id="2"><Ingredientadd /></span>
-        <span id="3"><Ingredientadd /></span>
-        <span id="4"><Ingredientadd /></span>
-        <span id="5"><Ingredientadd /></span>
-        <span id="6"><Ingredientadd /></span>
-        <span id="7"><Ingredientadd /></span>
-        <span id="8"><Ingredientadd /></span>
+        <span>{this.ingredRow()}</span>
+      </div>
+
+    );
+  }
+
+    /*
+  render() {
+    console.log(this.state.rows);
+    let className = "hidden";
+    return (
+      <div>
+        <form> 
+          <button type="submit" onClick={this.addRow}>[+]</button>
+          <button type="sumbit" onClick={this.deleteRow}>[-]</button>
+        </form>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
+        <span className={className}><Ingredientadd /></span>
       </div>
     );
   }
+
+*/
+
 }
 
 export default Plusminus;
