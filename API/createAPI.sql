d0fd19a7bf00eabe1ac39b0f20dfa114a1c57fdc
ntbloom <ntbloom@gmail.com>
2019-02-16 13:31:56 -0500

    first iteration of schema for new database organization

--- API/createAPI.sql
+++ API/createAPI.sql
@@ -1,30 +1,32 @@
 /* createAPI.sql -- creates SQL tables for drinkbase API */
 
-/*TODO: create table recipes if not exists
-name blob
-ingredients blob
-unit blob
-amount real
-primary key (name, ingredients)
-foreign key ingred (chemistry.ingredient)
-foreign key name (prep.name)
+--recipes: quantity of ingredients in each drink
+DROP TABLE IF EXISTS recipes;
+CREATE TABLE recipes (
+  name TEXT NOT NULL,
+  ingredient TEXT NOT NULL,
+  unit TEXT NOT NULL,
+  amount REAL NOT NULL,
+  PRIMARY KEY(name, ingredient)
+  FOREIGN KEY(ingredient) REFERENCES ingredients(ingredient)
+  FOREIGN KEY(name) REFERENCES build(name)
+);
 
---TODO: create table chemistry if not exists
-ingredients blob
-class blob
-abv float
-sugar float
-brightness float
-primary key (ingredients)
+--ingredients: info on each drink ingredient
+DROP TABLE IF EXISTS ingredients;
+CREATE TABLE ingredients (
+  ingredient TEXT NOT NULL PRIMARY KEY,
+  class TEXT NOT NULL,
+  alcohol REAL NOT NULL,
+  sweetness REAL NOT NULL,
+  brightness REAL NOT NULL
+);
 
---TODO: create table prep if not exists
-name blob
-style blob
-glass blob
-notes blob
-*/
-
-/* notes:
-chemistry.class = sweetener, juice, base,etc 
-prep.style = stirred, shaken, bubbly, built, eggs, etc.
-*/
+--build: how to build the drink
+DROP TABLE IF EXISTS build; 
+CREATE TABLE build (
+  name TEXT NOT NULL PRIMARY KEY,
+  style TEXT NOT NULL,
+  glass TEXT NOT NULL,
+  notes BLOB
+);
