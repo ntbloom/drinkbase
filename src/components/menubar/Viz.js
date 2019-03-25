02b80d51357ce38c8fb84e1ed11486b50ab969fc
ntbloom <ntbloom@gmail.com>
2019-03-24 21:46:38 -0400

    added all of S's code. Not working currently, need to find solution

--- src/components/menubar/Viz.js
+++ src/components/menubar/Viz.js
@@ -15,9 +15,13 @@ class Drinkviz extends Component {
     this.state = {
       //need to change to dynamic value
       picks: ["Blood & Sand", "Penicillin", "Scotch Old Fashioned","Scotch Toddy", "Bobby Burns", "Rob Roy", "Rusty Nail"],
+      showRecipeCounter: 0
     };
     // you need to bind your functions before declarations
     this.drawPlot = this.drawPlot.bind(this);
+    this.highlight = this.highlight.bind(this);
+    this.unhighlight = this.unhighlight.bind(this);
+    this.fillRecipe = this.fillRecipe.bind(this);
   }
   
   // gets called on first load
@@ -34,7 +38,7 @@ class Drinkviz extends Component {
     const drinks = this.props.drinks
     const picks = this.state.picks
 
-    let drinksSVG = d3.select('#theDrinks');
+    var drinksSVG = d3.select('#theDrinks');
     
     drinksSVG.append("line")
       .attr("x1",200)
@@ -73,7 +77,7 @@ class Drinkviz extends Component {
 
     drinksSVG.selectAll("circle").remove()
 
-    let abvCirc = drinksSVG.selectAll("#abvCircle")
+    var abvCirc = drinksSVG.selectAll("#abvCircle")
       .data(drinks.Drinks)
       .enter().append("circle")
       .attr("id", "abvCircle")
@@ -124,9 +128,107 @@ class Drinkviz extends Component {
               } else {
                   return "#bbb";
               }
-          })
-
+      
+      })
+    //.on("mouseover", this.highlight())
+    //.on("mouseout", this.unhighlight())
+    //.on("click", this.fillRecipe());
   }        
+  
+  // the tooltip functions
+  highlight(d,i) {
+    var circle = d3.select(this)
+      .attr("fill-opacity",1)
+      .attr("stroke-width",1.5);
+    
+    d3.select("#tooltip")
+        .style("left", d3.event.pageX + 5 + "px")
+        .style("top", d3.event.pageY - 30 + "px")
+        //.text(d.Name)
+        .style("visibility","visible");
+    
+    d3.select("#drinkName").text(d.Name);
+    d3.select("#drinkStyle").text(d.Data.Style);
+    d3.select("#drinkIngredients").text(d.Data.IngredientString);
+  }
+
+  unhighlight(d,i) {
+    var circle = d3.select(this)
+      .attr("fill-opacity", function(d) {
+        if (this.picks.includes(d.Name)) {
+          return 0.95
+        } else {
+          return 0.05
+        }
+      })
+      .attr ("stroke-width", function(d) {
+        if (this.picks.includes(d.Name)) {
+          return 0.75
+        } else {
+          return 0.2
+        }
+      });
+    d3.select("#tooltip")
+      .style("visibility","hidden");
+  }
+
+  showRecipe() {
+    this.showRecipeCounter += 1;
+    if (this.showRecipeCounter % 2 === 1) {
+        d3.select("#recipeBox")
+            .style("visibility","visible");
+    } else {
+        d3.select("#recipeBox")
+            .style("visibility","hidden");
+    }
+  }
+
+  fillRecipe(d) {
+    var thisName = d.Name
+    d3.select("#recipeTitle").text(thisName).style("font-weight", "bold");
+    d3.select("#recipeBox").selectAll('#recipeIng').remove();
+    d3.select("#recipeBox").selectAll('#recipeBody').remove();
+    var rec = d3.select("#recipeBox").selectAll("#recipeIng")
+      .data(this.props.drinks.Drinks.filter(function(d) { return d.Name === thisName })[0].Recipe)
+      .enter()
+      .append("div")
+      .attr('id', 'recipeIng')
+      .text(function(d) {
+          return d.Amount + ' ' + d.Unit + ' ' + d.Ingredient;
+      });
+
+    var inst = d3.select("#recipeBox").selectAll("#recipeBody")
+      .data(this.props.drinks.Drinks.filter(function(d) { return d.Name === thisName }))
+      .enter()
+      .append("p")
+      .attr('id', 'recipeBody')
+      .text(function(d) {
+    var prepDirections = '';
+       
+    // style
+    /*if (d.Notes.length > 0) {
+      prepDirections += d.Notes
+    } else */
+    if (d.Data.Style.includes("stirred")) {
+      prepDirections += "Combine ingredients in mixing glass and stir with ice until cold, then strain into glass.";
+    } else if (d.Data.Style.includes("bubbly")) {
+      prepDirections += "Stir all but bubbly with ice until cold. Strain and top with bubbly.";
+    } else if (d.Data.Style.includes("shaken")) {
+      prepDirections += "Combine ingredients in shaker and shake with ice until cold, then strain into glass.";
+    } else if (d.Data.Style.includes("fizz")) {
+      prepDirections += "Dry shake all ingredients (no ice) for 10 seconds. Then add ice and shake until cold. Strain into glass.";
+    } else if (d.Data.Style.includes("built")) {
+      prepDirections += "Build ingredients in serving glass, over ice. Stir and enjoy.";
+    } else if (d.Data.Style.includes("hot")) {
+      prepDirections += "Combine all ingredients and serve hot.";
+    }
+
+    //glass
+    prepDirections += ' Serve in a ' + d.Data.Glass + ' glass.';
+        
+    return prepDirections;
+      });
+    }
   
   render() {
     return (
