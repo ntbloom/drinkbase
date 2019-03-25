267f65f3eb0d3d09c567c44d377e062cfc7aab9d
ntbloom <ntbloom@gmail.com>
2019-03-24 21:19:48 -0400

    no errors on render but screen is blank

--- src/components/menubar/Viz.js
+++ src/components/menubar/Viz.js
@@ -31,28 +31,26 @@ class Drinkviz extends Component {
   }
   
   drawPlot() {
-    const node = this.node
     const drinks = this.props.drinks
     const picks = this.state.picks
 
-    d3.select(node)
-      .append("line")
+    let drinksSVG = d3.select('#theDrinks');
+    
+    drinksSVG.append("line")
       .attr("x1",200)
       .attr("x2",725)
       .attr("y1",300)
       .attr("y2",300)
       .attr("stroke","#888")
 
-    d3.select(node)
-      .append("line")
+    drinksSVG.append("line")
       .attr("x1",462)
       .attr("x2",462)
       .attr("y1",45)
       .attr("y2",560)
       .attr("stroke","#888")
 
-   d3.select(node) 
-      .append("text")
+    drinksSVG.append("text")
       .text("<- (less)        Sugar        (more) ->")
       .attr ("x",462)
       .attr ("y",615)
@@ -62,8 +60,7 @@ class Drinkviz extends Component {
       .style ("font-size","13pt")
       .attr ("opacity", 0.4)
         
-    d3.select(node)
-      .append("text")
+    drinksSVG.append("text")
       .text("<- (less)        Alcohol        (more) ->")
       .attr ("x",875)
       .attr ("y",310)
@@ -73,6 +70,62 @@ class Drinkviz extends Component {
       .style ("font-size","13pt")
       .attr ("opacity", 0.4)
       .attr('transform', 'rotate(270 875 295)')
+
+    drinksSVG.selectAll("circle").remove()
+
+    let abvCirc = drinksSVG.selectAll("#abvCircle")
+      .data(drinks.Drinks)
+      .enter().append("circle")
+      .attr("id", "abvCircle")
+      .attr ("stroke-width", function(d) {
+              if (picks.includes(d.Name)) {
+                  return 0.75
+              } else {
+                  return 0.2
+              }
+          })
+      .attr ("stroke", '#999')
+      .attr ("fill-opacity", function(d) {
+              if (picks.includes(d.Name)) {
+                  return 0.8
+              } else {
+                  return 0.1
+              }
+          })
+      .attr ("cx",function(d) { 
+          // TEMPORARY!!!!! trying to debug getting the spacing right
+          if (Math.max(30, (d.Data.Sweetness * 4000) - 25) < 100) {
+              console.log("TOO LOW: " + d.Name + " :: " + d.Data.Sweetness + " :: " + Math.max(30, (d.Data.Sweetness * 4000) - 25));
+          }
+          if (Math.max(30, (d.Data.Sweetness * 4000) - 25) > 800) {
+              console.log("TOO HIGH: " + d.Name + " :: " + d.Data.Sweetness + " :: " + Math.max(30, (d.Data.Sweetness * 4000) - 25));
+          }
+          // TEMPORARY!!! ^ trying to debug getting the spacing right
+          return Math.min(Math.max(30, (d.Data.Sweetness * 1600) - 25), 825);
+      })
+      .attr ("cy",function(d) { 
+          return Math.min(Math.max(30, (630 - (d.Data.AlcoholUnits * 400))), 570);
+      })
+      .attr ("r",function(d) { return (d.Data.Volume * Math.min(d.Data.Volume, 3)) + 3; })
+      .attr("fill",function(d) { 
+              if (d.Data.Style.includes("stirred")) {
+                  return "#a5693d";
+              } else if (d.Data.Style.includes("bubbly")) {
+                  return "#fcf5bf";
+              } else if (d.Data.Style.includes("shaken")) {
+                  return "#100656";
+                  //return "#240ccc";
+              } else if (d.Data.Style.includes("fizz")) {
+                  return "#f4e381";
+              } else if (d.Data.Style.includes("swizzle")) {
+                  return "#f27552";
+              }    else if (d.Data.Style.includes("built")) {
+                  return "#b5390c";
+              } else {
+                  return "#bbb";
+              }
+          })
+
   }        
   
   render() {
