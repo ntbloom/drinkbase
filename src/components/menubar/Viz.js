7a65eb4fb23221aa52e4991709b5f8447932d366
ntbloom <ntbloom@gmail.com>
2019-03-25 21:49:20 -0400

    deleted unnecessary recipe-serving methods

--- src/components/menubar/Viz.js
+++ src/components/menubar/Viz.js
@@ -21,7 +21,6 @@ class Drinkviz extends Component {
     this.drawPlot = this.drawPlot.bind(this);
     this.highlight = this.highlight.bind(this);
     this.unhighlight = this.unhighlight.bind(this);
-    this.fillRecipe = this.fillRecipe.bind(this);
   }
   
   // gets called on first load
@@ -130,7 +129,6 @@ class Drinkviz extends Component {
       })
     //.on("mouseover", this.highlight())
     //.on("mouseout", this.unhighlight())
-    //.on("click", this.fillRecipe());
   }        
   
   // the tooltip functions
@@ -170,74 +168,16 @@ class Drinkviz extends Component {
       .style("visibility","hidden");
   }
 
-  showRecipe() {
-    this.showRecipeCounter += 1;
-    if (this.showRecipeCounter % 2 === 1) {
-        d3.select("#recipeBox")
-            .style("visibility","visible");
-    } else {
-        d3.select("#recipeBox")
-            .style("visibility","hidden");
-    }
-  }
-
-  fillRecipe(d) {
-    var thisName = d.Name
-    d3.select("#recipeTitle").text(thisName).style("font-weight", "bold");
-    d3.select("#recipeBox").selectAll('#recipeIng').remove();
-    d3.select("#recipeBox").selectAll('#recipeBody').remove();
-    var rec = d3.select("#recipeBox").selectAll("#recipeIng")
-      .data(this.props.drinks.Drinks.filter(function(d) { return d.Name === thisName })[0].Recipe)
-      .enter()
-      .append("div")
-      .attr('id', 'recipeIng')
-      .text(function(d) {
-          return d.Amount + ' ' + d.Unit + ' ' + d.Ingredient;
-      });
-
-    var inst = d3.select("#recipeBox").selectAll("#recipeBody")
-      .data(this.props.drinks.Drinks.filter(function(d) { return d.Name === thisName }))
-      .enter()
-      .append("p")
-      .attr('id', 'recipeBody')
-      .text(function(d) {
-    var prepDirections = '';
-       
-    // style
-    /*if (d.Notes.length > 0) {
-      prepDirections += d.Notes
-    } else */
-    if (d.Data.Style.includes("stirred")) {
-      prepDirections += "Combine ingredients in mixing glass and stir with ice until cold, then strain into glass.";
-    } else if (d.Data.Style.includes("bubbly")) {
-      prepDirections += "Stir all but bubbly with ice until cold. Strain and top with bubbly.";
-    } else if (d.Data.Style.includes("shaken")) {
-      prepDirections += "Combine ingredients in shaker and shake with ice until cold, then strain into glass.";
-    } else if (d.Data.Style.includes("fizz")) {
-      prepDirections += "Dry shake all ingredients (no ice) for 10 seconds. Then add ice and shake until cold. Strain into glass.";
-    } else if (d.Data.Style.includes("built")) {
-      prepDirections += "Build ingredients in serving glass, over ice. Stir and enjoy.";
-    } else if (d.Data.Style.includes("hot")) {
-      prepDirections += "Combine all ingredients and serve hot.";
-    }
-
-    //glass
-    prepDirections += ' Serve in a ' + d.Data.Glass + ' glass.';
-        
-    return prepDirections;
-      });
-    }
-  
   render() {
     return (
       <div> 
         <h3 id="viz">Drinks</h3>
-        <div className='thePlot' onclick="showRecipe()">
+        <div className='thePlot' onclick="this.showRecipe()">
           <svg className="bigPlot" id="theDrinks" width="925" height="630"></svg>
         </div>
         <div id="tooltip" className="tooltip">
           <span id="drinkName"></span><br />  
-          <span id="drinkStyle"></span> :
+          <span id="drinkStyle"></span> 
           <span id="drinkIngredients"></span>
         </div>
         <div id="recipeBox">
