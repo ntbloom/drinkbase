70d9e12edef32bc4011b74072c1bee3eaa840621
ntbloom <ntbloom@gmail.com>
2019-03-31 21:49:58 -0400

    ready to fix api calls

--- src/components/menubar/results/Results.js
+++ src/components/menubar/results/Results.js
@@ -10,51 +10,38 @@ class Results extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      haveDrinks: false,
-      drinks: '',
+      received: false, 
     };
     this.apiCall = this.apiCall.bind(this);
-    this.getDrinks = this.getDrinks.bind(this);
-  }
-  apiCall(url, stateVariable) {
-    // calls Flask API
-    fetch(url, {credentials: "include"})
-      .then(response => {
-        return response.json();
-      })
-      .then(drinks => {
-        return drinks; 
-      })
-      .then(stateful => {
-        this.setState({stateVariable: true});
-      })
   }
 
-  getDrinks() {
-    // calls Python api
-    const api = this.props.url;
+  apiCall() {
+    // calls Flask API
+    let api = this.props.url;
     const url = api.concat(this.props.query);
-    const drinks = this.apiCall(url, "haveDrinks");
-    if (this.state.haveDrinks) {
-      this.setState({drinks: drinks});
-      this.setState({submitted: true});
-    }
+    fetch(url, {credentials: "include"})
+      .then(response => {
+        return response;
+      })
+      .then(drinks => {
+        this.setState({drinks: drinks, received: true}); 
+      })
   }
 
   componentDidMount() {
     //console.log("url: ", url);
-    this.getDrinks();
+    this.apiCall();
     console.log("this.state.drinks:", this.state.drinks);
   }
   componentDidUpdate(prevProps) {
     if (this.props.query !== prevProps.query) {
-      this.getDrinks();
+      this.apiCall();
     }
   }
   render() {
     //console.log("drinks: ", this.state.drinks.Drinks);
     //console.log("query: ", this.props.query);
-    if (this.state.submitted) {
+    if (this.state.received) {
       return (
         <div>
           <Drinklist drinks={this.state.drinks.Drinks} />
