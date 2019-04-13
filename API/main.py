00fbb870b4788fd2e3486ce4455a8a191c3ff370
ntbloom <ntbloom@gmail.com>
2019-04-12 22:47:05 -0400

    ran pylint over main.py

--- API/main.py
+++ API/main.py
@@ -1,59 +1,47 @@
 #!/usr/bin/python3
-# api.py - python/flask script for running drinkBase back end
+''' api.py - python/flask script for running drinkBase back end'''
 
 from store.pgStore import DrinkBase
-from flask import Flask, request, Response, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 
-ds = DrinkBase('drinkbase')
-app = Flask(__name__)
+DS = DrinkBase('drinkbase')
+APP = Flask(__name__)
 
 
 
-@app.route('/api/v1.0/ingreds/', methods=['GET'])
-#querying the database by ingredient
+@APP.route('/api/v1.0/ingreds/', methods=['GET'])
 def ingreds():
+    '''querying the database by ingredient'''
     incl = request.args.get('incl')
     excl = request.args.get('excl')
-    drinks = ds.allDrinks
+    drinks = DS.allDrinks
     if incl:
         incl = incl.split(',')
         for i in incl:
-            tempSet = set(ds.ingSearch(i))
+            tempSet = set(DS.ingSearch(i))
             drinks = drinks & tempSet
     if excl:
         excl = excl.split(',')
         for i in excl:
-            tempSet = set(ds.ingSearch(i))
+            tempSet = set(DS.ingSearch(i))
             drinks = drinks - tempSet
     drinks = sorted(list(drinks))
-    drinks = ds.sendRecipe(drinks)
+    drinks = DS.sendRecipe(drinks)
 
-    return drinks 
-
-@app.route('/api/v1.0/names/', methods=['GET'])
-#querying the database by drink name
-def names():
-    name = request.args.get('name')
-    drinks = ds.nameSearch(name)
-    drinks = ds.sendRecipe(drinks)
     return drinks
 
-#CORS headers. Uncomment for local development.
+@APP.route('/api/v1.0/names/', methods=['GET'])
+def names():
+    '''querying the database by drink name'''
+    name = request.args.get('name')
+    drinks = DS.nameSearch(name)
+    drinks = DS.sendRecipe(drinks)
+    return drinks
 
-'''
-@app.after_request
-def make_cors(response):
-    response.headers = {
-            "Access-Control-Allow-Origin": "http://localhost:3000",
-            "Access-Control-Allow-Credentials": "true"
-            }
-    return response
-'''
-
-@app.errorhandler(404)
-
-def not_found(error):
+@APP.errorhandler(404)
+def not_found():
+    '''returns 404 page'''
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    APP.run(debug=True, use_reloader=False)
