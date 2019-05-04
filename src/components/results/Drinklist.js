eda1ff50af5c1b084feb09bd5320f94221aa0e7f
ntbloom <ntbloom@gmail.com>
2019-05-04 14:14:42 -0400

    have circles beside drink name with same color as drinkviz circles

--- src/components/results/Drinklist.js
+++ src/components/results/Drinklist.js
@@ -6,12 +6,21 @@ import React, { Component } from "react";
 import Recipe from "./Recipe";
 
 function pullIngreds(obj) {
+  // converts ingredient object into an array
   let ingredients = [];
   for (let i = 0; i < obj.length; i++) {
     ingredients.push(" " + obj[i]["Ingredient"]);
   }
   ingredients = ingredients.toString();
   return ingredients;
+}
+
+function getColor(drink) {
+  // dynamically renders colors based on glass
+  let style = drink.Data.Style;
+  style = style.charAt(0).toUpperCase() + style.slice(1);
+  const color = "var(--viz".concat(style);
+  return color;
 }
 
 class Drinklist extends Component {
@@ -32,7 +41,7 @@ class Drinklist extends Component {
       const listItems = drinks.map(drink => (
         <li id="drinkname" key={drinks.indexOf(drink).toString()}>
           <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
-            <circle id="glass" cx="50%" cy="50%" r="7" fill="red" />
+            <circle id="glass" cx="50%" cy="50%" r="7" fill={getColor(drink)} />
           </svg>
           {drink.Name}
           <span id="ingreds">{pullIngreds(drink.Recipe)}</span>
