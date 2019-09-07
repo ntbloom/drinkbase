ce869dc06e34076bfedfaea7fba98d8889cf1e6d
ntbloom <ntbloom@gmail.com>
2019-09-07 14:24:52 -0400

    added shell script for pretty printing log outlets

---
+++ deployment/nginx/printlogs.sh
@@ -0,0 +1,13 @@
+DEST_DIR="/var/log/nginx/"
+
+echo "recent IP addresses and timestamps"
+grep -h chunk $DEST_DIR/* | sort | sed 's/].*//'
+
+echo "\nUnique IPs in regular logs"
+grep -s chunk $DEST_DIR/* | sed -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*$/\1/' | sort | uniq | wc -l
+
+echo "\nUnique IPs in zipped files"
+gunzip -cq $DEST_DIR/*.gz | grep -s chunk | sed -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*$/\1/' | sort | uniq | wc -l
+
+echo "\nRecords date back to"
+date -r $DEST_DIR/access.log.14.gz
