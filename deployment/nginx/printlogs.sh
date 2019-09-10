e76422fda60ed0020800603eefab560ad804f396
ntbloom <ntbloom@gmail.com>
2019-09-10 12:41:11 -0400

    added timestamp to log printout

--- deployment/nginx/printlogs.sh
+++ deployment/nginx/printlogs.sh
@@ -1,13 +1,13 @@
 DEST_DIR="/var/log/nginx/"
 
-echo "recent IP addresses and timestamps"
+printf "IP access since "
+date -r $DEST_DIR/access.log.2.gz
 grep -h chunk $DEST_DIR/* | sort | cut -c 1-36 | sed 's/$/]/' | uniq
 
 echo "\nUnique IPs in regular logs"
 grep -s chunk $DEST_DIR/* | sed -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*$/\1/' | sort | uniq | wc -l
 
-echo "\nUnique IPs in zipped files"
+printf "\nUnique IPs in zipped files since "
+date -r $DEST_DIR/access.log.14.gz
 gunzip -cq $DEST_DIR/*.gz | grep -s chunk | sed -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*$/\1/' | sort | uniq | wc -l
 
-echo "\nRecords date back to"
-date -r $DEST_DIR/access.log.14.gz
