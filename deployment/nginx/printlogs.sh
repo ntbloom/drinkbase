4a640e6a654f6ac11cc94a32b1cb7c5639bac2e6
ntbloom <ntbloom@gmail.com>
2019-10-11 21:17:37 -0400

    fixed npm vulnerabilities

--- deployment/nginx/printlogs.sh
+++ deployment/nginx/printlogs.sh
@@ -2,7 +2,7 @@ DEST_DIR="/var/log/nginx/"
 
 printf "IP access since "
 date -r $DEST_DIR/access.log.2.gz
-grep -h chunk $DEST_DIR/* | sort | cut -c 1-36 | sed 's/$/]/' | uniq
+grep -h chunk $DEST_DIR/* | sort | cut -c 1-38 | sed 's/$/]/' | uniq
 
 echo "\nUnique IPs in regular logs"
 grep -s chunk $DEST_DIR/* | sed -e 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*$/\1/' | sort | uniq | wc -l
