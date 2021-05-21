8bd22f282686d4772c6abbfdfc3ef2ae5ca813ca
ntbloom <ntbloom@gmail.com>
2021-05-20 22:41:34 -0400

    moved all tests to pytest

--- backend/test/test_api.py
+++ backend/test/test_api.py
@@ -2,6 +2,7 @@
 
 import requests
 import pytest
+from typing import List, Union
 
 
 BASE_URL = "http://localhost:8080/api/v1.0/"
@@ -42,21 +43,9 @@ NO_JUICE_BITTERS = [
 ]
 
 
-def drinkViz(drink, keyword):
-    url = BASE_URL + "names/?name=" + drink
-    r = requests.get(url)
-    drinks = r.json()["Drinks"]
-    value = drinks[0]["Data"][keyword]
-    if keyword == ing or keyword == sty or keyword == gar:
-        value = value
-    else:
-        value = round(value, 3)
-    return value
-
-
 class TestDrinkBase:
     @staticmethod
-    def api(url):
+    def _check_api_names(url: str) -> List[str]:
         """Test the API endpoint"""
         r = requests.get(BASE_URL + url)
         drinks = r.json()["Drinks"]
@@ -65,8 +54,21 @@ class TestDrinkBase:
             drinklist.append(i["Name"])
         return sorted(drinklist)
 
+    @staticmethod
+    def _check_api_values(drink: str, keyword: str) -> Union[float, str]:
+
+        url = BASE_URL + "names/?name=" + drink
+        r = requests.get(url)
+        drinks = r.json()["Drinks"]
+        value = drinks[0]["Data"][keyword]
+        if keyword == ing or keyword == sty or keyword == gar:
+            value = value
+        else:
+            value = round(value, 3)
+        return value
+
     @pytest.mark.parametrize(
-        "api_args,expected_result",
+        "api_args,expected_names",
         [
             ("names/?name=bitter french", ["Bitter French"]),
             ("names/?name=negroni", ["Negroni", "Negroni Sbagliato", "Summer Negroni"]),
@@ -94,119 +96,48 @@ class TestDrinkBase:
             ("ingreds/?incl=heering&excl=", ["Blood & Sand", "Singapore Sling"]),
         ],
     )
-    def test_api(self, api_args, expected_result):
-        assert self.api(api_args) == expected_result
+    def test_api_names(self, api_args: str, expected_names: List[str]):
+        """Do we get the proper drink names for each query?"""
+        assert self._check_api_names(api_args) == expected_names
 
-
-#         )
-#
-#     # drinkViz tests
-#     def test_abv(self):
-#         self.assertEqual(drinkViz("Negroni", abv), 0.224)
-#
-#     def test_alcohol_units(self):
-#         self.assertEqual(drinkViz("Negroni", alc), 0.805)
-#
-#     def test_brightness(self):
-#         self.assertEqual(drinkViz("Negroni", bri), 0.006)
-#
-#     def test_garnish(self):
-#         self.assertEqual(drinkViz("Negroni", gar), "orange twist")
-#
-#     def test_ingredient_string(self):
-#         self.assertEqual(drinkViz("Negroni", ing), "Campari | gin | sweet vermouth")
-#
-#     def test_style(self):
-#         self.assertEqual(drinkViz("Negroni", sty), "built")
-#
-#     def test_sweetness(self):
-#         self.assertEqual(drinkViz("Negroni", swe), 0.4)
-#
-#     def test_volume(self):
-#         self.assertEqual(drinkViz("Negroni", vol), 3.6)
-#
-#     # TODO: write
-#     """
-#     def test_abv_(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', abv), '??')
-#
-#     def test_alcohol_units(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', alc), '??')
-#
-#     def test_brightness(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', bri), '??')
-#
-#     def test_garnish(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', gar), '??')
-#
-#     def test_ingredient_string(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', ing),
-#             '??')
-#
-#     def test_style(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', sty), '??')
-#
-#     def test_sweetness(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', swe), '??')
-#
-#     def test_volume(self):
-#         self.assertEqual(drinkViz('Ramos Gin Fizz', vol), '??')
-#     """
-#
-#     def test_abv(self):
-#         self.assertEqual(drinkViz("Margarita", abv), 0.192)
-#
-#     def test_alcohol_units(self):
-#         self.assertEqual(drinkViz("Margarita", alc), 1.1)
-#
-#     def test_brightness(self):
-#         self.assertEqual(drinkViz("Margarita", bri), 0.06)
-#
-#     def test_garnish(self):
-#         self.assertEqual(drinkViz("Margarita", gar), "salt rim and lime wedge")
-#
-#     def test_ingredient_string(self):
-#         self.assertEqual(
-#             drinkViz("Margarita", ing),
-#             "Cointreau | lime juice | agave nectar | tequila reposado",
-#         )
-#
-#     def test_style(self):
-#         self.assertEqual(drinkViz("Margarita", sty), "shaken")
-#
-#     def test_sweetness(self):
-#         self.assertEqual(drinkViz("Margarita", swe), 0.454)
-#
-#     def test_volume(self):
-#         self.assertEqual(drinkViz("Margarita", vol), 5.737)
-#
-#     def test_abv(self):
-#         self.assertEqual(drinkViz("Martinez", abv), 0.238)
-#
-#     def test_alcohol_units(self):
-#         self.assertEqual(drinkViz("Martinez", alc), 0.837)
-#
-#     def test_brightness(self):
-#         self.assertEqual(drinkViz("Martinez", bri), 0.006)
-#
-#     def test_garnish(self):
-#         self.assertEqual(drinkViz("Martinez", gar), "lemon twist")
-#
-#     def test_ingredient_string(self):
-#         self.assertEqual(
-#             drinkViz("Martinez", ing),
-#             "Angostura bitters | Luxardo Maraschino | gin | sweet vermouth",
-#         )
-#
-#     def test_style(self):
-#         self.assertEqual(drinkViz("Martinez", sty), "stirred")
-#
-#     def test_sweetness(self):
-#         self.assertEqual(drinkViz("Martinez", swe), 0.218)
-#
-#     def test_volume(self):
-#         self.assertEqual(drinkViz("Martinez", vol), 3.521)
-#
-#
-# if __name__ == "__main__":
-#     unittest.main()
+    @pytest.mark.parametrize(
+        "name,param,expected_value",
+        [
+            ("Negroni", abv, 0.224),
+            ("Negroni", abv, 0.224),
+            ("Negroni", alc, 0.805),
+            ("Negroni", bri, 0.006),
+            ("Negroni", gar, "orange twist"),
+            ("Negroni", ing, "Campari | gin | sweet vermouth"),
+            ("Negroni", sty, "built"),
+            ("Negroni", swe, 0.4),
+            ("Negroni", vol, 3.6),
+            ("Margarita", abv, 0.192),
+            ("Margarita", alc, 1.1),
+            ("Margarita", bri, 0.06),
+            ("Margarita", gar, "salt rim and lime wedge"),
+            (
+                "Margarita",
+                ing,
+                "Cointreau | agave nectar | lime juice | tequila reposado",
+            ),
+            ("Margarita", sty, "shaken"),
+            ("Margarita", swe, 0.454),
+            ("Margarita", vol, 5.737),
+            ("Martinez", abv, 0.238),
+            ("Martinez", alc, 0.837),
+            ("Martinez", bri, 0.006),
+            ("Martinez", gar, "lemon twist"),
+            (
+                "Martinez",
+                ing,
+                "Angostura bitters | Luxardo Maraschino | gin | sweet vermouth",
+            ),
+            ("Martinez", sty, "stirred"),
+            ("Martinez", swe, 0.218),
+            ("Martinez", vol, 3.521),
+        ],
+    )
+    def test_api_values(self, name: str, param: str, expected_value: Union[float, str]):
+        """Do we get the right results for various queries?"""
+        assert self._check_api_values(name, param) == expected_value
