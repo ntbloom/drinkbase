5cb321fe2017fef811a2e427d3da2dfc4572093e
ntbloom <ntbloom@gmail.com>
2021-06-02 22:33:27 -0400

    including build directory

---
+++ frontend/build/precache-manifest.af51d7d43d94bf86fd7dde774634fc3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3505bed47a93cf778198798db450d218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c30f805443332c366d91",
+    "url": "/static/css/main.5a8dde8b.chunk.css"
+  },
+  {
+    "revision": "983c79cb8b139c54aa28",
+    "url": "/static/js/2.969b2cc0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.969b2cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30f805443332c366d91",
+    "url": "/static/js/main.26faf40c.chunk.js"
+  },
+  {
+    "revision": "1cb93187b04d74d8e953",
+    "url": "/static/js/runtime-main.f774bc60.js"
+  },
+  {
+    "revision": "7e01f26e48cb9b9f636d3a8243fc2985",
+    "url": "/static/media/drinkBaseWhite.7e01f26e.png"
+  }
+]);
