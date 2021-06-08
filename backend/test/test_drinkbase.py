ded0b548c0f38cd27a6edd3d7d44a201f78eb53f
ntbloom <ntbloom@gmail.com>
2021-06-07 22:17:52 -0400

    docker-compose fixture for postgresql

--- backend/test/test_drinkbase.py
+++ backend/test/test_drinkbase.py
@@ -1,34 +1,66 @@
-from typing import List
 import subprocess
 import logging
+from pathlib import Path
+from time import sleep
+
 
 import docker
-from docker import DockerClient
 import pytest
 
 from backend.src.drinkbase import DrinkBase
 
 
 @pytest.fixture
-def random_list() -> List[str]:
-    pass
+def drinkbase() -> DrinkBase:
+    return DrinkBase("drinkbase")
 
 
 @pytest.fixture(scope="session", autouse=True)
-def database():
+def database(log):
     """spin up the database"""
-    client = docker.from_env()
-    # TODO: spin up database
+    docker_dir = Path(__file__).parent.parent.parent.joinpath("docker")
+    log.info(docker_dir)
+    up = subprocess.run(
+        "docker-compose up -d db", shell=True, cwd=docker_dir, capture_output=True
+    )
+    up.check_returncode()
+    connected = False
+    timeout = 30
+    while not connected and timeout > 0:
+        connected = (
+            subprocess.run(
+                "nc -vz localhost 5432", shell=True, capture_output=True
+            ).returncode
+            == 0
+        )
+        timeout -= 1
+        sleep(1)
+        log.info(timeout)
+    assert connected, "Failure to connect to postgresql"
+    yield
+    subprocess.run("docker-compose down", shell=True, cwd=docker_dir)
 
 
-@pytest.fixture
-def log(caplog):
+@pytest.fixture(scope="session")
+def log():
     logger = "drinkbase"
-    caplog.set_level(logging.DEBUG, logger=logger)
     return logging.getLogger(logger)
 
 
 class TestDrinkbase:
     def test_database_fixture(self, log):
+        """assert database is up"""
         client = docker.from_env()
-        log.debug(client.containers.list())
+        containers = client.containers.list()
+        found = False
+        for container in containers:
+            if container.name == "docker_db_1":
+                found = True
+                break
+        assert found
+
+    def test_all_drinks(self, drinkbase: DrinkBase, log):
+        """Test all drinks"""
+        drinks = drinkbase.sendAllDrinks()
+        log.info(drinks)
+        assert "Negroni" in drinks
